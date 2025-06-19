f7e989009914d0a84ccd2ceae60f4b6f4a1da7c8
Ruckstar <caraledso@gmail.com>
2025-06-18 23:36:25 -0600

    Creada y añadida la carpeta de vistas para autenticación y primeros pasos de autenticación.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 
                 <nav class=" flex gap-2">
                     <a class="font-bold uppercase text-gray-600" href="#">Login</a>
-                    <a class="font-bold uppercase text-gray-600" href="#">Crear Cuenta</a>
+                    <a class="font-bold uppercase text-gray-600" href="/crear-cuenta">Crear Cuenta</a>
                     <a href=""></a>
                     <a href=""></a>
                 </nav>
